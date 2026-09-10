2f3138bea70b9c51460f3dc714aaa2842bdfe585
문성현 <7sonicx@gmail.com>
2026-09-10 09:58:31 +0900

    chore: 프로젝트 초기 설정

---
+++ src/main/java/me/seonghyeon/projectspringboot1/ProjectSpringboot1Application.java
@@ -0,0 +1,13 @@
+package me.seonghyeon.projectspringboot1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjectSpringboot1Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ProjectSpringboot1Application.class, args);
+    }
+
+}
---
+++ src/test/java/me/seonghyeon/projectspringboot1/ProjectSpringboot1ApplicationTests.java
@@ -0,0 +1,13 @@
+package me.seonghyeon.projectspringboot1;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjectSpringboot1ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
